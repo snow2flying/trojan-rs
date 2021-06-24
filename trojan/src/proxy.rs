e621a1cd0977883d6a6fc68a1299319bfc7ced96
git <snow2flying@gmail.com>
2021-06-24 00:07:03 -0600

    add udp associate support

--- trojan/src/proxy.rs
+++ trojan/src/proxy.rs
@@ -1,7 +1,6 @@
-use std::{io::Error as IoError, sync::Arc, fmt};
+use std::{io::Error as IoError, sync::Arc, fmt, io};
 
 use event_listener::Event;
-use futures_lite::AsyncWriteExt;
 use futures_util::io::AsyncReadExt;
 use futures_util::stream::StreamExt;
 use log::debug;
@@ -17,14 +16,15 @@ type TerminateEvent = Arc<Event>;
 use crate::authenticator::{Authenticator, NullAuthenticator};
 use errors::Result;
 use std::net::{SocketAddr, IpAddr, Ipv4Addr, SocketAddrV4, Ipv6Addr, SocketAddrV6};
-use futures_util::AsyncRead;
+use futures_util::{AsyncRead, AsyncWrite, AsyncWriteExt};
 use std::io::Cursor;
 use bytes::{Buf, BufMut};
 use errors::Error;
 use std::fmt::{Debug, Formatter};
-use async_std::net::{TcpStream, TcpListener};
+use async_std::net::{TcpStream, TcpListener, UdpSocket};
 use async_std::task::spawn;
-
+use futures_util::future::Either;
+use futures_util::FutureExt;
 
 type SharedAuthenticator = Arc<Box<dyn Authenticator>>;
 
@@ -149,7 +149,56 @@ enum RequestHeader {
     TcpConnect([u8; HASH_STR_LEN], Address),
     UdpAssociate([u8; HASH_STR_LEN]),
 }
+/// ```plain
+/// +------+----------+----------+--------+---------+----------+
+/// | ATYP | DST.ADDR | DST.PORT | Length |  CRLF   | Payload  |
+/// +------+----------+----------+--------+---------+----------+
+/// |  1   | Variable |    2     |   2    | X'0D0A' | Variable |
+/// +------+----------+----------+--------+---------+----------+
+/// ```
+pub struct UdpHeader {
+    pub address: Address,
+    pub payload_len: u16,
+}
 
+impl UdpHeader {
+    #[inline]
+    pub fn new(addr: &Address, payload_len: usize) -> Self {
+        Self {
+            address: addr.clone(),
+            payload_len: payload_len as u16,
+        }
+    }
+
+    pub async fn read_from<R>(stream: &mut R) -> Result<Self>
+        where
+            R: AsyncRead + Unpin,
+    {
+        let addr = Address::read_from_stream(stream).await?;
+        let mut buf = [0u8; 2];
+        stream.read_exact(&mut buf).await?;
+        let len = ((buf[0] as u16) << 8) | (buf[1] as u16);
+        stream.read_exact(&mut buf).await?;
+        log::debug!("udp addr={} len={}", addr, len);
+        Ok(Self {
+            address: addr,
+            payload_len: len,
+        })
+    }
+
+    pub async fn write_to<W>(&self, w: &mut W) -> Result<()>
+        where
+            W: AsyncWrite + Unpin,
+    {
+        let mut buf = Vec::with_capacity(self.address.serialized_len() + 2 + 1);
+        let cursor = &mut buf;
+        self.address.write_to_buf(cursor);
+        cursor.put_u16(self.payload_len);
+        cursor.put_slice(b"\r\n");
+        w.write_all(&buf).await?;
+        Ok(())
+    }
+}
 const CMD_TCP_CONNECT: u8 = 0x01;
 const CMD_UDP_ASSOCIATE: u8 = 0x03;
 
@@ -159,7 +208,7 @@ async fn proxy(
     authenticator: SharedAuthenticator,
 ) -> Result<()> {
     use crate::copy::copy;
-    use fluvio_future::task::spawn;
+    // use fluvio_future::task::spawn;
 
 
     let mut hash_buf = [0u8; HASH_STR_LEN];
@@ -238,8 +287,84 @@ async fn proxy(
             // Ok(RequestHeader::TcpConnect(hash_buf, addr))
         },
         CMD_UDP_ASSOCIATE => {
-            std::process::exit(1);
             debug!("UdpAssociate target addr: {:?}",addr);
+
+
+            const RELAY_BUFFER_SIZE: usize = 0x4000;
+            let outbound = UdpSocket::bind(SocketAddr::from(SocketAddrV6::new(
+                Ipv6Addr::UNSPECIFIED,
+                0,
+                0,
+                0,
+            )))
+                .await?;
+            let (mut tls_stream_reader, mut tls_stream_writer) = tls_stream.split();
+
+            let client_to_server = Box::pin(async {
+                loop {
+                    let mut buf = [0u8; RELAY_BUFFER_SIZE];
+                    let header = UdpHeader::read_from(&mut tls_stream_reader).await?;
+
+                    tls_stream_reader
+                        .read_exact(&mut buf[..header.payload_len as usize])
+                        .await?;
+
+
+
+                    match outbound.send_to(&buf[..header.payload_len as usize], header.address.to_string()).await {
+                        Ok(n) => {
+                            // if n == 0 {
+                            //     tls_socket.clone().close().await?;
+                            // }
+                        }
+                        Err(e) => {
+                            // tls_socket.clone().close().await?;
+                            error!("udp send to upstream error: {:?}", e);
+                            break;
+                        }
+                    }
+
+
+                }
+                Ok(()) as Result<()>
+            })
+                .fuse();
+            let server_to_client = Box::pin(async {
+                let mut buf = [0u8; RELAY_BUFFER_SIZE];
+                loop {
+                    let (len, dst) = outbound.recv_from(&mut buf).await?;
+                    if len == 0 {
+                        // tls_socket.clone().close().await?;
+                        break;
+                    }
+                    let header = UdpHeader::new(&Address::from(dst), len);
+                    header.write_to(&mut tls_stream_writer).await?;
+                    tls_stream_writer.write_all(&buf[..len]).await?;
+                }
+                tls_stream_writer.flush().await?;
+                tls_stream_writer.close().await?;
+                Ok(()) as Result<()>
+            })
+                .fuse();
+            let res = futures::future::select(client_to_server, server_to_client).await;
+            match res {
+                Either::Left((Err(e), _)) => {
+                    debug!("udp copy to remote closed");
+                    Err(anyhow::anyhow!("====================UdpAssociate copy local to remote error: {:?}=================",e))?
+                }
+                Either::Right((Err(e), _)) => {
+                    debug!("udp copy to local closed");
+                    Err(anyhow::anyhow!("====================UdpAssociate copy remote to local error: {:?}==================",e))?
+                }
+                Either::Left((Ok(_), _)) | Either::Right((Ok(_), _)) => (),
+            };
+
+
+
+
+
+
+
             // Ok(RequestHeader::UdpAssociate(hash_buf))
         },
         _ => {
@@ -397,6 +522,28 @@ impl Address {
             }
         }
     }
+    fn write_to_buf<B: BufMut>(&self, buf: &mut B) {
+        match self {
+            Self::SocketAddress(SocketAddr::V4(addr)) => {
+                buf.put_u8(Self::ADDR_TYPE_IPV4);
+                buf.put_slice(&addr.ip().octets());
+                buf.put_u16(addr.port());
+            }
+            Self::SocketAddress(SocketAddr::V6(addr)) => {
+                buf.put_u8(Self::ADDR_TYPE_IPV6);
+                for seg in &addr.ip().segments() {
+                    buf.put_u16(*seg);
+                }
+                buf.put_u16(addr.port());
+            }
+            Self::DomainNameAddress(domain_name, port) => {
+                buf.put_u8(Self::ADDR_TYPE_DOMAIN_NAME);
+                buf.put_u8(domain_name.len() as u8);
+                buf.put_slice(&domain_name.as_bytes()[..]);
+                buf.put_u16(*port);
+            }
+        }
+    }
 }
 
 impl Debug for Address {
@@ -416,5 +563,10 @@ impl fmt::Display for Address {
             Address::SocketAddress(ref addr) => write!(f, "{}", addr),
             Address::DomainNameAddress(ref addr, ref port) => write!(f, "{}:{}", addr, port),
         }
+    }
+}
+impl From<SocketAddr> for Address {
+    fn from(s: SocketAddr) -> Address {
+        Address::SocketAddress(s)
     }
 }
